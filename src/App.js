edad0fc7e3904874d770267e2dc0e3481b273232
luisdb50 <luisdanieldb4@gmail.com>
2021-10-01 18:54:04 -0400

    estructura header semi-realizada

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Page from "./components/Page";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Page />
     </div>
   );
 }
