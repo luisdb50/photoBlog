33aba94e006900354a3dca438df2642bb773ec34
luisdb50 <luisdanieldb4@gmail.com>
2021-10-07 18:41:31 -0400

    Cont About finalizado

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,4 +1,8 @@
+import about_img from "../img/about.jpg";
+import gallery_img from "../img/gallery.svg";
+
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 export default class Content extends React.Component{
     
@@ -11,9 +15,36 @@ export default class Content extends React.Component{
 
     render(){
         return(
-            <div className="content_body">
-                
-            </div>
+            <Container fluid className="content_body">
+                <Row className="body_about">
+                <img className="img_style" src={about_img} alt="about"/>
+                    <Col className="col_flex" xs={12} sm={11} md={11}>
+                        <Col sm={6} md={5} className="cont_about">
+                            <div className="cont_img_about">
+                                <img className="img_style2" src={gallery_img} alt="gallery"/>
+                            </div>
+                            <p className="text_about">
+                                Guarda tus imagenes favoritas de una manera facil y eficiente
+                                con PhotoBlog. Aqui podras tener tu propia galeria con las imagenes
+                                que jamas quieras perder en todos tus dispositivos.
+                            </p>
+                        </Col>                        
+                    </Col>
+                </Row>
+                <Row className="content_features">
+                    <Col>
+
+                    </Col>
+
+                    <Col>
+
+                    </Col>
+
+                    <Col>
+                        
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
+import img_logo from "../img/logo.png"
+
 import React from "react";
-import { Navbar } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 
 
 export default function Header(props) {
@@ -7,14 +9,27 @@ export default function Header(props) {
 
     return(
         <header className="content_header">
-            <Navbar bg="dark">
-              <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
-              <Navbar.Toggle />
-              <Navbar.Collapse className="justify-content-end">
-                <Navbar.Text>
-                  Signed in as: <a href="#login">Mark Otto</a>
-                </Navbar.Text>
-              </Navbar.Collapse>
+            <Navbar expand="sm" className="nav_content">
+                <Navbar.Brand href="#home">
+                    <img
+                        alt="logo"
+                        src={img_logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top sp"
+                    />
+                    <p className="ptitle">PhotoBlog</p>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                
+                <Navbar.Collapse className="justify-content-end">
+                    <Nav className="nav_links">
+                        <Nav.Link href="#home">Registrate</Nav.Link>
+                        <Nav.Link href="#home">Inicio</Nav.Link>
+                        <Nav.Link href="#link">Galeria</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                
             </Navbar>
         </header>
     );
