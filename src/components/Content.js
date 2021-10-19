f8ae347b0895cee41c735c925f31871b9b7ffba3
luisdb50 <luisdanieldb4@gmail.com>
2021-10-19 00:51:06 -0400

    Cambio clases por funciones

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,43 +7,54 @@ import view_img from "../img/view.png";
 import fea_img from "../img/features.jpg";
 import prof_img from "../img/profile.png";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Container, Row, Table } from "react-bootstrap";
 import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import { onAuthStateChanged } from "firebase/auth";
 
-export default class Content extends React.Component{
+export default function Content(){
     
-    constructor(props){
-        super(props);
-        this.state = {
-            resultRequest: "",
-            load: false,
-            active: false,
-            user: null
-        }
+    const [request, setRequest] = useState(null);
+    const [active, setActive] = useState(false);
+    const [load, setLoad] = useState(false);
+    const [user, setUser] = useState(null);
 
-        this.handleRequest = this.handleRequest.bind(this);
-        this.handleAuth = this.handleAuth.bind(this);
-        this.handleLogout = this.handleLogout.bind(this);
-    }
+    let loading =
+            <div className="cont_figure_inactive">
+                <div className="figure">
+                    <div className="figure2">
+                        <div className="figure3">
+                        </div>
+                    </div>
+                </div>
+                <p className="text_load">Cargando...</p>
+            </div>;
 
-    componentDidMount(){
+    let users =
+        <Container onClick={()=>setActive(false)} fluid className="cont_emergent">
+            <div className="div_emergent">
+                {load === false ? loading : tableResultRequest()}
+            </div>
+        </Container>;
+
+    useEffect(()=>{
         const auth = getAuth();
         onAuthStateChanged(auth, user => {
-            this.setState({ user });
+            setUser(user);
         });
-    }
+    });
 
-    handleRequest(){
-        this.setState({active: true});
+    function handleRequest(){
+        setActive(true);
         fetch('https://jsonplaceholder.typicode.com/users/1/todos')
             .then(response => response.json())
-            .then(json => this.setState({resultRequest: json, load: true}))
+            .then(json => {
+                setRequest(json); setLoad(true);
+            })
             .catch(error => console.log(error));
     }
 
-    handleAuth(){
+    function handleAuth(){
         const provider = new GoogleAuthProvider();
 
         const auth = getAuth();
@@ -52,26 +63,26 @@ export default class Content extends React.Component{
             .catch(error => console.log("Error " + error))
     }
 
-    handleLogout(){
+    function handleLogout(){
         const auth = getAuth();
         signOut(auth)
-            .then(result => console.log(result.user.email + " ha salido"))
+            .then(result => console.log("has salido"))
             .catch(error => console.log("Error " + error))
     }
 
-    renderLoginButton(){
+    function renderLoginButton(){
             // Si el usuario esta logueado
-        if(this.state.user){
+        if(user){
             return(
                 <Col md={5} className="center_prof">
                     <img 
                         className="img_prof" 
-                        src={this.state.user.photoURL} 
-                        alt={this.state.user.displayName}
+                        src={user.photoURL} 
+                        alt={user.displayName}
                     />
 
-                    <p>Hola {this.state.user.displayName}</p>
-                    <button onClick={this.handleLogout}>Salir</button>
+                    <p>Hola {user.displayName}</p>
+                    <button onClick={handleLogout}>Salir</button>
                 </Col>
             );
         } else {
@@ -79,13 +90,13 @@ export default class Content extends React.Component{
                 <Col md={5} className="center_prof">
                     <img className="img_prof" src={prof_img} alt="profile"/>
                     <p>Inicia Sesion</p>
-                    <button onClick={this.handleAuth}>Login con Google</button>
+                    <button onClick={handleAuth}>Login con Google</button>
                 </Col>
             );
         }
     }
 
-    tableResultRequest(){
+    function tableResultRequest(){
         return(
             <Table striped hover>
                 <thead>
@@ -97,7 +108,7 @@ export default class Content extends React.Component{
                 </thead>
                 <tbody>
                 {
-                    this.state.resultRequest.slice(0, 10).map((value) => 
+                request.slice(0, 10).map((value) => 
                     <tr key={value.id}>
                         <td>{value.id}</td>
                         <td>{value.title}</td>
@@ -109,105 +120,82 @@ export default class Content extends React.Component{
         );
     }
 
-    
-
-    render(){
-
-        let loading =
-            <div className="cont_figure_inactive">
-                <div className="figure">
-                    <div className="figure2">
-                        <div className="figure3">
-                        </div>
-                    </div>
-                </div>
-                <p className="text_load">Cargando...</p>
-            </div>;
-
-        let users =
-            <Container onClick={()=>this.setState({active: false})} fluid className="cont_emergent">
-                <div className="div_emergent">
-                    {this.state.load === false ? loading : this.tableResultRequest()}
-                </div>
-            </Container>;
-        
-        return(
-            <div>
-                <Container fluid className="content_body" id="home">
-                    <Row className="body_about" id="about">
-                        <img className="bg_about_img" src={about_img} alt="about"/>
-                        <Col className="col_flex" xs={12} sm={11} md={11}>
-                            <Col sm={6} md={5} className="cont_about">
-                                <div className="cont_img_about">
-                                    <img className="img_style2" src={gallery_img} alt="galeria"/>
-                                </div>
-                                <p className="text_about">
-                                    Bienvenido a PhotoBlog. Has click en el boton de solicitar si quieres
-                                    ver las primeras diez personas que se han registrado
-                                    en PhotoBlog.
-                                </p>
-                                <button onClick={this.handleRequest}>solicitar</button>
-                            </Col>                        
-                        </Col>
-                    </Row>
-                    <Row className="body_features" id="features">
-                        <Col xs={8} sm={5} lg={3}>
-                            <img className="img_style3" src={archive_img} alt="archivo" />
-                            <p className="title_fea">Protege</p>
-                            <p className="text_features">
-                                Manten protegidas tus imagenes con PhotoBlog contra la
-                                perdida de datos. Una vez almacena la imagen en tu galeria
-                                nunca la perderas de vista.
+    return(
+        <div>
+            <Container fluid className="content_body" id="home">
+                <Row className="body_about" id="about">
+                    <img className="bg_about_img" src={about_img} alt="about"/>
+                    <Col className="col_flex" xs={12} sm={11} md={11}>
+                        <Col sm={6} md={5} className="cont_about">
+                            <div className="cont_img_about">
+                                <img className="img_style2" src={gallery_img} alt="galeria"/>
+                            </div>
+                            <p className="text_about">
+                                Bienvenido a PhotoBlog. Has click en el boton de solicitar si quieres
+                                ver las primeras diez personas que se han registrado
+                                en PhotoBlog.
                             </p>
-                        </Col>
-                        <Col xs={8} sm={5} lg={3}>
-                            <img className="img_style3" src={view_img} alt="vista" />
-                            <p className="title_fea">Galeria</p>
-                            <p className="text_features">
-                                Disfruta de una elegante galeria donde podras visualizar
-                                todas las imagenes que has subido a una excelente calidad
-                            </p>
-                        </Col>
-                        <Col xs={8} sm={5} lg={3}>
-                            <img className="img_style3" src={device_img} alt="dispositivo" />
-                            <p className="title_fea">Compatibilidad</p>
+                            <button onClick={handleRequest}>solicitar</button>
+                        </Col>                        
+                    </Col>
+                </Row>
+                <Row className="body_features" id="features">
+                    <Col xs={8} sm={5} lg={3}>
+                        <img className="img_style3" src={archive_img} alt="archivo" />
+                        <p className="title_fea">Protege</p>
+                        <p className="text_features">
+                            Manten protegidas tus imagenes con PhotoBlog contra la
+                            perdida de datos. Una vez almacena la imagen en tu galeria
+                            nunca la perderas de vista.
+                        </p>
+                    </Col>
+                    <Col xs={8} sm={5} lg={3}>
+                        <img className="img_style3" src={view_img} alt="vista" />
+                        <p className="title_fea">Galeria</p>
+                        <p className="text_features">
+                            Disfruta de una elegante galeria donde podras visualizar
+                            todas las imagenes que has subido a una excelente calidad
+                        </p>
+                    </Col>
+                    <Col xs={8} sm={5} lg={3}>
+                        <img className="img_style3" src={device_img} alt="dispositivo" />
+                        <p className="title_fea">Compatibilidad</p>
 
-                            <p className="text_features">
-                                PhotoBlog al ser una aplicacion web esta disponible para
-                                todo dispositivo que contenga un navegador instalado asi que
-                                podras acceder a tus imagenes desde tu pc, telefono, smartTv
-                                y muchos dispositivos mas.
-                            </p> 
-                        </Col>
-                        <Col xs={8} sm={5} lg={3}>
-                            <img className="img_style3" src={format_img} alt="formato" />
-                            <p className="title_fea">Diferentes formatos</p>
-                            <p className="text_features">
-                                Sube diferentes tipos de archivos de imagenes gracias a la
-                                gran compatibilidad con los diferentes formatos que admite PhotoBlog
-                            </p>
-                        </Col>
-                    <img className="bg_fea_img" src={fea_img} alt="about"/>
-                    </Row>
-                </Container>
-                {this.state.active ? users : <div></div>}
-                
-                <Container fluid className="cont_login">
-                
-                    <Row className="justify-content-md-center">
-                        <Col>
-                            <p className="title_signin">Inicia sesion con Google para comenzar</p>
-                            <p>
-                                Necesitas iniciar sesion con google para acceder a tu galeria de
-                                imagenes. Has click en el boton de login para iniciar sesion
-                            </p>
-                        </Col>
-                        {this.renderLoginButton()}
-                    </Row>
-                    
-                </Container>
-            </div>
+                        <p className="text_features">
+                            PhotoBlog al ser una aplicacion web esta disponible para
+                            todo dispositivo que contenga un navegador instalado asi que
+                            podras acceder a tus imagenes desde tu pc, telefono, smartTv
+                            y muchos dispositivos mas.
+                        </p> 
+                    </Col>
+                    <Col xs={8} sm={5} lg={3}>
+                        <img className="img_style3" src={format_img} alt="formato" />
+                        <p className="title_fea">Diferentes formatos</p>
+                        <p className="text_features">
+                            Sube diferentes tipos de archivos de imagenes gracias a la
+                            gran compatibilidad con los diferentes formatos que admite PhotoBlog
+                        </p>
+                    </Col>
+                <img className="bg_fea_img" src={fea_img} alt="about"/>
+                </Row>
+            </Container>
+            {active ? users : <div></div>}
             
-        );
-    }
+            <Container fluid className="cont_login">
+            
+                <Row className="justify-content-md-center">
+                    <Col>
+                        <p className="title_signin">Inicia sesion con Google para comenzar</p>
+                        <p className="text_signin">
+                            Necesitas iniciar sesion con google para acceder a tu galeria de
+                            imagenes. Has click en el boton de login para iniciar sesion
+                        </p>
+                    </Col>
+                    {renderLoginButton()}
+                </Row>
+                
+            </Container>
+        </div>
+        
+    );
 }
--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,35 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { getStorage, ref, uploadBytesResumable } from "firebase/storage";
 
-export class FileUpload extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            uploadValue: 0,
-            picture: null
-        };
+export function FileUpload(){
+    
+    const [uploadValue, setUploadValue] = useState(0);
+    const [picture, setPicture] = useState(null);
 
-        this.handleUpload = this.handleUpload.bind(this);
-    }
-
-    handleUpload(event){
-        const file = event.target.files['0'];
+    function handleUpload(e){
+        const file = e.target.files['0'];
         const storage = getStorage();
         const storageRef = ref(storage, `fotos/${file.name}`);
         
         uploadBytesResumable(storageRef, file)
-            .then(snapshot => { console.log(event);});
+            .then(snapshot => { console.log(e);});
     }
     
-    render(){
-        return(
-            <div>
-                <progress value={this.state.uploadValue} max="100"></progress>
-                <br/>
-                <input type="file" onChange={this.handleUpload}/>
-                <br/>
-                <img width="320" src={this.state.picture} alt="" />
-            </div>
-        );
-    }
+    return(
+        <div>
+            <progress value={uploadValue} max="100"></progress>
+            <br/>
+            <input type="file" onChange={handleUpload()}/>
+            <br/>
+            <img width="320" src={picture} alt="" />
+        </div>
+    );
 }
